216f4e465d055c3a5bde2776408e2171cd2e1ead
smky0311 <std08219@gmail.com>
2025-11-14 11:03:38 +0900

    메인 함수 재정의 및 초기용 test fuction 정의

--- 2025_Cprogramming_teamA/main.c
+++ 2025_Cprogramming_teamA/main.c
@@ -1,10 +1,22 @@
-//main
 #include <stdio.h>
 
+//본인 이름으로 함수만들어서 메인에 출력(함수원형 꼭 정의하기)
+//**네이밍 user_'본인이름영어로' 하기**
+void user_jihyung();
+
+//main
 int main(void) {
-	print("Hello");
-	print("World");
+	//본인이름 함수 호출
+	user_jihyung();
+	//printf...
+	
 	return 0;
 }
+//함수정의
+void user_jihyung() {
+	printf("김지형");
+}
+//void user...
+
 
 //for test
