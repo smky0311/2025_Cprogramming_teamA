aa8930d4d9fe2c0f1fbf4a51bc6068e3f1083ac8
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2025-11-24 21:42:10 +0900

    Added signup function.

--- 2025_Cprogramming_teamA/user.c
+++ 2025_Cprogramming_teamA/user.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-#include "common.h" // 전역변수(g_users) 사용을 위해 필수
+#include "common.h"
 #include "user.h"
 
-// 팀원 1이 아래 세 함수 담당
 void loadUsers() {
-    // TODO: users.dat 파일 읽어서 g_users에 채우기
-    printf("[System] 회원 데이터를 로드했습니다.\n");
+    printf("[System]\n");
 }
 
 void saveUsers() {
-    // TODO: g_users 내용을 users.dat에 저장하기
-    printf("[System] 회원 데이터를 저장했습니다.\n");
+    printf("[System]\n");
 }
 
 void manageUsers() {
-    // TODO: 회원 목록 출력 구현
-    printf("[Admin] 회원 관리 메뉴 진입...\n");
+    printf("[Admin]\n");
 }
